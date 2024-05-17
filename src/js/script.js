123aa9856adfa432207093b712fea16a075213f5
kaleem-uet <kaleemullahk989@gmail.com>
2024-05-17 10:34:32 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// Add a click event listener to the button
+document.getElementById("lessons").addEventListener("click", function () {
+    window.location.href = "lesson.html";
+});
+document.getElementById('assessment').addEventListener("click", function () {
+    window.location.href = 'assessment.html';
+});
+document.getElementById('result').addEventListener("click", function () {
+    window.location.href = 'result.html';
+})
+document.getElementById('feedback').addEventListener("click", function () {
+    window.location.href = 'feedback.html';
+})
+
